56e72b398bb8942a229062ae2a8edaebdc49141a
Viktorya Hakobyan <victoria@webmaker.am>
2021-06-12 18:02:57 +0400

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3532de6cc101bfc8d3b87d650a870e36'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2d0ee356-a539-4172-b7b0-5a3e08daab38'
+};
